29c3f60d1432d7a95c1c0897e6bca515e8aa449c
sahilhedau49 <hedausahil49@gmail.com>
2023-09-08 00:27:14 +0530

    Firestore POST and Fetch Done

---
+++ client/src/Hooks/useFirestore.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
+import { db } from "../Firebase";
+
+const useFirestore = () => {
+  const [res, setRes] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    try {
+      const q = query(
+        collection(db, "documents"),
+        orderBy("createdAt", "desc")
+      );
+      onSnapshot(q, (querySnapshot) => {
+        const d = [];
+        querySnapshot.forEach((doc) => {
+          const u = doc.data().docUrl;
+          const c = doc.data().createdAt.toDate();
+          const g = doc.data().ownerEmail;
+          d.push({ u, c, g });
+        });
+        setRes(d);
+        setIsloading(false);
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+  return { res, isLoading };
+};
+
+export default useFirestore;
--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,22 +1,31 @@
 import React from "react";
+import useFirestore from "../Hooks/useFirestore";
 
 const Gallery = () => {
+  const { res, isLoading } = useFirestore();
+  console.log(res);
+
   return (
-    <div className="grid md:grid-cols-3 justify-center gap-4 mt-10">
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img
-            className="w-20"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png"
-            alt="PDF Img"
-          />
-        </figure>
-        <div className="card-body">
-          <p>Uploaded By: </p>
-          <p>Created On: </p>
+    <>
+      <div className="mt-6 text-center">
+        {isLoading && <progress className="progress w-56"></progress>}
+      </div>
+      <div className="grid md:grid-cols-3 justify-center gap-4 mt-10">
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              className="w-20"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png"
+              alt="PDF Img"
+            />
+          </figure>
+          <div className="card-body">
+            <p>Uploaded By: </p>
+            <p>Created On: </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
